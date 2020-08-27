56646fa242184c9efbac922a74fc3012507cca1a
mikerobbins04 <mikerobbins04@yahoo.com>
2020-08-26 21:21:23 -0500

    Updates

---
+++ precache-manifest.526875a9b5eeccd6b16854146bb970e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f7819f1af839638e56f0907874951e",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "7ca32dce9b5a0d45643c",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7ca32dce9b5a0d45643c",
+    "url": "/contactmanager/static/js/2.c3fbc56d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.c3fbc56d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004ae06e1a853e006bb0",
+    "url": "/contactmanager/static/js/main.ba368e06.chunk.js"
+  },
+  {
+    "revision": "98d49c782551fbed3aca",
+    "url": "/contactmanager/static/js/runtime-main.53501af3.js"
+  }
+]);
